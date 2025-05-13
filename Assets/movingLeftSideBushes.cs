8ad1456fd3e84443500113dca1f8bb6e13976b48
Boitumelo274 <2878260@students.wits.ac.za>
2025-05-13 20:11:38 +0200

    working on movement
    i was busy with movement of the road

--- Assets/movingLeftSideBushes.cs
+++ Assets/movingLeftSideBushes.cs
@@ -1,14 +1,10 @@
-
-
-
-
 using UnityEngine;
 
-public class LoopingToControllTheRoad : MonoBehaviour
+public class movingLeftSideBushes : MonoBehaviour
 {
+    public float scrollSpeed = 2f;
     public float resetY = 20f;
     public float startY = 20f;
-    public float scrollSpeed = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,15 +14,15 @@ public class LoopingToControllTheRoad : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         transform.Translate(Vector3.down * scrollSpeed * Time.deltaTime);
-        if(transform.position.y<= -resetY)
+        if (transform.position.y <= -resetY)
         {
             Vector3 pos = transform.position;
             pos.y = pos.y + 2 * resetY;
             transform.position = pos;
-
-           // scrollSpeed = scrollSpeed + 2f;
         }
+
+
     }
 }
-
